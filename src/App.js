05f80947bacccf0b5b8d04bea3c06631b2bee537
QochinyanTigran <09355022tigo@gmail.com>
2019-07-16 12:33:43 +0400

    project settings

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Icon from '@material-ui/core/Icon';
+
+function App() {
+  return (
+    <div className="App">
+        hellow everyone
+        <Icon>add_circle</Icon>
+    </div>
+  );
+}
+
+export default App;
