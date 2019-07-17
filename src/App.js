2db9193cd29209532c9b9ae69a59a2e68d42c1db
QochinyanTigran <09355022tigo@gmail.com>
2019-07-17 15:46:12 +0400

    first skillet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import Icon from '@material-ui/core/Icon';
+
+import Home from './comonents/Home/Home';
+import SectionTitle from './comonents/SectionTitle/SectionTitle';
+import Footer from './comonents/Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-        hellow everyone
-        <Icon>add_circle</Icon>
-    </div>
-  );
+    return (
+      	<div className="App">
+        	<Home id='Home' slogan='aaaaaaaa' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, assumenda!'/>
+			<div id='' className='slider'></div>
+			
+			<Footer />
+		</div>
+  	);
 }
 
 export default App;
